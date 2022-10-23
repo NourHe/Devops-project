f2f5ea7c8b399438206a3fe7df5de4dd7595cba2
youssef.tfifha <youssef.tfifha@esprit.tn>
2022-10-23 17:06:42 +0100

    FIX:Mock File

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplMock.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplMock.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
-class ProduitServiceImplMockTest{
+class ProduitServiceImplMock{
     @Mock
     ProduitRepository produitRepository;
     @InjectMocks
