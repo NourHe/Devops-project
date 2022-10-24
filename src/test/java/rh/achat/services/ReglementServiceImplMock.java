d80eff69abbfa03b3027ccf26b0d8dc0c626fcb8
NourHe <47791837+NourHe@users.noreply.github.com>
2022-10-24 16:25:40 +0200

    mockito Test 1

--- src/test/java/rh/achat/services/ReglementServiceImplMock.java
+++ src/test/java/rh/achat/services/ReglementServiceImplMock.java
@@ -4,11 +4,14 @@ package rh.achat.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.rh.achat.AchatApplication;
 import tn.esprit.rh.achat.entities.Reglement;
 import tn.esprit.rh.achat.repositories.ReglementRepository;
 
@@ -20,15 +23,18 @@ import java.util.List;
 import java.util.Optional;
 
 
-@SpringBootTest
+@SpringBootTest(classes=AchatApplication.class)
 @ExtendWith(MockitoExtension.class)
 public class ReglementServiceImplMock {
 	@Mock
-    ReglementRepository regRepository;
+    ReglementRepository reglementRepository;
     @InjectMocks
-    ReglementServiceImpl RegImp;
-
-    Reglement regl = new Reglement(10.5f, 3.2f,Boolean.TRUE, new Date());
+    ReglementServiceImpl reglementService;
+    
+    
+   
+    Reglement regl1 = new Reglement(10.5f, 3.2f,Boolean.TRUE, new Date());
+   
     List<Reglement> listReglement = new ArrayList<Reglement>() {
         {
             add(new Reglement(17.5f, 8.2f,Boolean.TRUE, new Date()));
@@ -38,8 +44,33 @@ public class ReglementServiceImplMock {
 
     @Test
     void retrieveReglement() {
-        Mockito.when(regRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(regl));
-        Reglement reg1 = RegImp.retrieveReglement(0L);
+        Mockito.when(reglementRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(regl1));
+        Reglement reg1 = reglementService.retrieveReglement(0L);
         Assertions.assertNotNull(reg1);
     }
+    
+    
+    @Test
+    void addReglement() {
+    	Reglement regl2 = new Reglement(20.520f, 320.20f,Boolean.TRUE, new Date());
+        Mockito.when(reglementRepository.save(ArgumentMatchers.any(Reglement.class))).thenReturn(regl2);
+        Reglement created = reglementService.addReglement(regl2);
+        Assertions.assertSame(created,regl2);
+        Mockito.verify(reglementRepository).save(regl2);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
