830c3e4b0f092bf09fc71e4f987bcf9fd6663282
youssef.tfifha <youssef.tfifha@esprit.tn>
2022-10-26 17:05:27 +0100

    update:AssignProductStock Mock Test

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplMock.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplMock.java
@@ -10,7 +10,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.ProduitRepository;
+import tn.esprit.rh.achat.repositories.StockRepository;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -24,8 +26,13 @@ class ProduitServiceImplMock{
     ProduitRepository produitRepository;
     @InjectMocks
     ProduitServiceImpl produitService;
-
     Produit produit= new Produit(1L,"P1","libelleP1",12.5f,new Date(),new Date());
+
+    @Mock
+    StockRepository stockRepository;
+    @InjectMocks
+    StockServiceImpl StI;
+    Stock stock=new Stock("S1",2,2);
     List<Produit> produitList=new ArrayList<Produit>(){
         {
         add(new Produit(2L,"P2","libelleP2",7.2f,new Date(),new Date()));
@@ -61,12 +68,22 @@ class ProduitServiceImplMock{
         Mockito.verify(produitRepository).save(Mockito.any(Produit.class));
     }
     @Test
-    void deleteOperateurTestM() {
+    void deleteProduit() {
 
         Produit produit = new Produit(4L,"P4","libelleP4",10f,new Date(),new Date());
         Mockito.lenient().when(produitRepository.findById(produit.getIdProduit())).thenReturn(Optional.of(produit));
         produitService.deleteProduit(produit.getIdProduit());
         Mockito.verify(produitRepository).deleteById(produit.getIdProduit());
+    }
+    @Test
+    void assignProduitToStock() {
+        Mockito.when(produitRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(produit));
+        Produit produit = produitService.retrieveProduit(1L);
+        Assertions.assertNotNull(produit);
+
+        Mockito.when(stockRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(stock));
+        Stock stock1 = StI.retrieveStock(0L);
+        Assertions.assertNotNull(stock1);
     }
 
 }
