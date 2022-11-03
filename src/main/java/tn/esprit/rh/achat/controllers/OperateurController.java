4ebd69778599571446c1964c60a114be5e00ac01
Fatma Abrougui <fatma.abrougui@esprit.tn>
2022-11-03 20:00:25 +0100

    99th change

--- src/main/java/tn/esprit/rh/achat/controllers/OperateurController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/OperateurController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.services.IOperateurService;
-
 import java.util.List;
 
 @RestController
