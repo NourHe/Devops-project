01db5aea6563c18b7b9452c752b783bbe134d91f
youssef.tfifha <youssef.tfifha@esprit.tn>
2022-11-13 19:00:44 +0100

    Fix:Jacoco

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplMockTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplMockTest.java
@@ -3,25 +3,21 @@ package tn.esprit.rh.achat.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.Produit;
-import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.ProduitRepository;
-import tn.esprit.rh.achat.repositories.StockRepository;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
-class ProduitServiceImplMock{
+class ProduitServiceImplMockTest {
 
     private Produit produit ;
     @Mock
